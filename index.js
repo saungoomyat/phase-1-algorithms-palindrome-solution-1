c5c2de82f18cf3872438043a0e8df10903addf6e
Saung Oo Myat <saung.myat@gmail.com>
2022-03-24 23:08:50 -0400

    commit tree f4e7b5497fa04e9012e387bdf2c255f1b473c158

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function reverseString(word) {
+  return word.split("").reverse().join("");
 }
 
+function isPalindrome(word) {
+  // reverse the input string
+  const reversedWord = reverseString(word);
+  // compare the reversed string to the input
+  return word === reversedWord;
+}
 /* 
   Add your pseudocode here
 */
@@ -12,9 +18,28 @@ function isPalindrome(word) {
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
 
   console.log("");
 
